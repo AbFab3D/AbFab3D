b8339a0fe33090b0c857baa61233d7a2876ef20f
Alan Hudson <giles7777@gmail.com>
2011-04-24 14:03:17 -0700

    Updated license headers to LGPL

--- src/java/abfab3d/geom/GeometryCache.java
+++ src/java/abfab3d/geom/GeometryCache.java
@@ -1,8 +1,9 @@
 /*****************************************************************************
- *                        Alan Hudson Copyright (c) 2011
+ *                        Shapeways, Inc Copyright (c) 2011
  *                               Java Source
  *
- * This source is private and not licensed for any use.
+ * This source is licensed under the GNU LGPL v2.1
+ * Please read http://www.gnu.org/copyleft/lgpl.html for more information
  *
  * This software comes with the standard NO WARRANTY disclaimer for any
  * purpose. Use it at your own risk. If there's a problem you get to fix it.
