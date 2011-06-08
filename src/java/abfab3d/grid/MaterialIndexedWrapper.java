f43e6591918e9e7b26b36af41e75861e252c9235
Alan Hudson <giles7777@gmail.com>
2011-06-08 16:57:15 -0700

    Alan: new wrapper

---
+++ src/java/abfab3d/grid/MaterialIndexedWrapper.java
@@ -0,0 +1,327 @@
+/*****************************************************************************
+ *                        Shapeways, Inc Copyright (c) 2011
+ *                               Java Source
+ *
+ * This source is licensed under the GNU LGPL v2.1
+ * Please read http://www.gnu.org/copyleft/lgpl.html for more information
+ *
+ * This software comes with the standard NO WARRANTY disclaimer for any
+ * purpose. Use it at your own risk. If there's a problem you get to fix it.
+ *
+ ****************************************************************************/
+
+package abfab3d.grid;
+
+// External Imports
+import java.util.*;
+import org.j3d.geom.GeometryData;
+
+// Internal Imports
+
+/**
+ * A wrapper that keeps a mapping of material to voxel coordinates.
+ * This allows for fast traversal/removal of material groups.
+ *
+ * This class trades memory for speed.  Given N voxels and E exterior
+ * voxels.  Usually exterior voxels = 1% of total voxels.  This
+ * class will use E * (sizeof(pointer) + sizeof(VoxelCoordinate)) == 20
+ * bytes extra to index.
+ *
+ * Insert operations will be slower(~100%).  Removal and traversal operations
+ * will be much faster(2E verses N^3).
+ *
+ * @author Alan Hudson
+ */
+public class MaterialIndexedWrapper implements GridWrapper {
+    /** The wrapper grid */
+    private Grid grid;
+
+    /**
+     * Constructor.
+     *
+     * @param grid The grid to wrap
+     */
+    public MaterialIndexedWrapper(Grid grid) {
+        this.grid = grid;
+    }
+
+    /**
+     * Remove all voxels associated with the Material.
+     *
+     * @param mat The materialID
+     */
+    public void removeMaterial(byte mat) {
+    }
+
+    /**
+     * Add a geometry associated with a materialID.
+     *
+     * @param geom The geometry
+     * @param mat The materialID
+     */
+    public void addGeometry(GeometryData geom, byte mat) {
+    }
+
+    //----------------------------------------------------------
+    // GridWrapper methods
+    //----------------------------------------------------------
+
+    /**
+     * Sets the underlying grid to use.
+     *
+     * @param grid The grid or null to clear.
+     */
+    public void setGrid(Grid grid) {
+        this.grid = grid;
+    }
+
+    //----------------------------------------------------------
+    // Grid methods
+    //----------------------------------------------------------
+
+    /**
+     * Get the data for a voxel
+     *
+     * @param x The x world coordinate
+     * @param y The y world coordinate
+     * @param z The z world coordinate
+     * @param The voxel data
+     */
+    public VoxelData getData(double x, double y, double z) {
+        return grid.getData(x,y,z);
+    }
+
+    /**
+     * Get the state of the voxel.
+     *
+     * @param x The x grid coordinate
+     * @param y The y grid coordinate
+     * @param z The z grid coordinate
+     * @param The voxel state
+     */
+    public VoxelData getData(int x, int y, int z) {
+        return grid.getData(x,y,z);
+    }
+
+    /**
+     * Get the state of the voxel
+     *
+     * @param x The x world coordinate
+     * @param y The y world coordinate
+     * @param z The z world coordinate
+     * @param The voxel state
+     */
+    public byte getState(double x, double y, double z) {
+        return grid.getState(x,y,z);
+    }
+
+    /**
+     * Get the state of the voxel.
+     *
+     * @param x The x grid coordinate
+     * @param y The y grid coordinate
+     * @param z The z grid coordinate
+     * @param The voxel state
+     */
+    public byte getState(int x, int y, int z) {
+        return grid.getState(x,y,z);
+    }
+
+    /**
+     * Get the state of the voxel
+     *
+     * @param x The x world coordinate
+     * @param y The y world coordinate
+     * @param z The z world coordinate
+     * @param The voxel material
+     */
+    public byte getMaterial(double x, double y, double z) {
+        return grid.getMaterial(x,y,z);
+    }
+
+    /**
+     * Get the material of the voxel.
+     *
+     * @param x The x grid coordinate
+     * @param y The y grid coordinate
+     * @param z The z grid coordinate
+     * @param The voxel material
+     */
+    public byte getMaterial(int x, int y, int z) {
+        return grid.getMaterial(x,y,z);
+    }
+
+    /**
+     * Set the value of a voxel.
+     *
+     * @param x The x world coordinate
+     * @param y The y world coordinate
+     * @param z The z world coordinate
+     * @param state The value.  0 = nothing. > 0 materialID
+     * @param material The materialID
+     */
+    public void setData(double x, double y, double z, byte state, byte material) {
+        byte old_state = grid.getState(x,y,z);
+
+        if (old_state != Grid.OUTSIDE && state != Grid.OUTSIDE) {
+            throw new IllegalArgumentException("Invalid state change");
+        }
+
+        grid.setData(x,y,z,state,material);
+    }
+
+    /**
+     * Set the value of a voxel.
+     *
+     * @param x The x world coordinate
+     * @param y The y world coordinate
+     * @param z The z world coordinate
+     * @param val The value.  0 = nothing. > 0 materialID
+     */
+    public void setData(int x, int y, int z, byte state, byte material) {
+        byte old_state = grid.getState(x,y,z);
+
+        if (old_state != Grid.OUTSIDE && state != Grid.OUTSIDE) {
+            throw new IllegalArgumentException("Invalid state change");
+        }
+
+        grid.setData(x,y,z,state,material);
+    }
+
+    /**
+     * Get the grid coordinates for a world coordinate.
+     *
+     * @param x The x value in world coords
+     * @param y The y value in world coords
+     * @param z The z value in world coords
+     * @param coords The ans is placed into this preallocated array(3).
+     */
+    public void getGridCoords(double x, double y, double z, int[] coords) {
+        grid.getGridCoords(x,y,z,coords);
+    }
+
+    /**
+     * Get the world coordinates for a grid coordinate.
+     *
+     * @param x The x value in grid coords
+     * @param y The y value in grid coords
+     * @param z The z value in grid coords
+     * @param coords The ans is placed into this preallocated array(3).
+     */
+    public void getWorldCoords(int x, int y, int z, double[] coords) {
+        grid.getWorldCoords(x,y,z,coords);
+    }
+
+    /**
+     * Get the grid bounds in world coordinates.
+     *
+     * @param min The min coordinate
+     * @param max The max coordinate
+     */
+    public void getGridBounds(double[] min, double[] max) {
+        grid.getGridBounds(min,max);
+    }
+
+    /**
+     * Traverse a class of voxels types.  May be much faster then
+     * full grid traversal for some implementations.
+     *
+     * @param vc The class of voxels to traverse
+     * @param t The traverer to call for each voxel
+     */
+    public void find(VoxelClasses vc, ClassTraverser t) {
+        grid.find(vc, t);
+    }
+
+    /**
+     * Count a class of voxels types.  May be much faster then
+     * full grid traversal for some implementations.
+     *
+     * @param vc The class of voxels to traverse
+     * @return The number
+     */
+    public int findCount(VoxelClasses vc) {
+        return grid.findCount(vc);
+    }
+
+    /**
+     * Traverse a class of material types.  May be much faster then
+     * full grid traversal for some implementations.
+     *
+     * @param mat The material to traverse
+     * @param t The traverer to call for each voxel
+     */
+    public void find(byte mat, ClassTraverser t) {
+        grid.find(mat, t);
+    }
+
+    /**
+     * Count a class of material types.  May be much faster then
+     * full grid traversal for some implementations.
+     *
+     * @param mat The class of material to traverse
+     * @return The number
+     */
+    public int findCount(byte mat) {
+        return grid.findCount(mat);
+    }
+
+    /**
+     * Get the number of height cells.
+     *
+     * @return the val
+     */
+    public int getHeight() {
+        return grid.getHeight();
+    }
+
+    /**
+     * Get the number of width cells.
+     *
+     * @return the val
+     */
+    public int getWidth() {
+        return grid.getWidth();
+    }
+
+    /**
+     * Get the number of depth cells.
+     *
+     * @return the val
+     */
+    public int getDepth() {
+        return grid.getDepth();
+    }
+
+    /**
+     * Get the voxel size in xz.
+     *
+     * @return The value
+     */
+    public double getVoxelSize() {
+        return grid.getVoxelSize();
+    }
+
+    /**
+     * Get the slice height.
+     *
+     * @return The value
+     */
+    public double getSliceHeight() {
+        return grid.getSliceHeight();
+    }
+
+    /**
+     * Print out a slice of data.
+     */
+    public String toStringSlice(int s) {
+        return grid.toStringSlice(s);
+    }
+
+    /**
+     * Print out all slices.
+     */
+    public String toStringAll() {
+        return grid.toStringAll();
+    }
+}
