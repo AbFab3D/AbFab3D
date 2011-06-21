0ee590a151691480c8d71576f746ba1586b24885
Alan Hudson <giles7777@gmail.com>
2011-06-21 15:48:34 -0700

    Alan: new voxel data formats

---
+++ src/java/abfab3d/grid/VoxelDataShort.java
@@ -0,0 +1,78 @@
+/*****************************************************************************
+ *                        Shapeways, Inc Copyright (c) 2011
+ *                               Java Source
+ *
+ * This source is licensed under the GNU LGPL v2.1
+ * Please read http://www.gnu.org/copyleft/lgpl.html for more information
+ *
+ * This software comes with the standard NO WARRANTY disclaimer for any
+ * purpose. Use it at your own risk. If there's a problem you get to fix it.
+ *
+ ****************************************************************************/
+
+package abfab3d.grid;
+
+/**
+ * Contains the data portion of a voxel.
+ *
+ * @author Alan Hudson
+ */
+public class VoxelDataShort implements VoxelData {
+    /** The voxel state */
+    private byte state;
+
+    /** The material */
+    private short material;
+
+    public VoxelDataShort(byte state, short material) {
+        this.state = state;
+        this.material = material;
+    }
+
+    /**
+     * Get the state.
+     *
+     * @return The state
+     */
+    public byte getState() {
+        return state;
+    }
+
+    /**
+     * Get the material.
+     *
+     * @return The material
+     */
+    public int getMaterial() {
+        return material;
+    }
+
+    /**
+     * Set the state.
+     *
+     * @param state The new state
+     */
+    public void setState(byte state) {
+        this.state = state;
+    }
+
+    /**
+     * Set the material.
+     *
+     * @param mat The material
+     */
+    public void setMaterial(int mat) {
+        this.material = (short) mat;
+    }
+
+    /**
+     * Set the state and material
+     *
+     * @param state The state
+     * @param mat The material
+     */
+    public void setData(byte state, int mat) {
+        this.state = state;
+        this.material = (short) mat;
+    }
+}
