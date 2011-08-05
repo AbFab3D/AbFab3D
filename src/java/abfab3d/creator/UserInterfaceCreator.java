112cd87c0ab89d8465ab9bc5c7a2510d80b0508a
Alan Hudson <giles7777@gmail.com>
2011-08-05 14:22:56 -0700

    Add file dialog support

---
+++ src/java/abfab3d/creator/UserInterfaceCreator.java
@@ -0,0 +1,35 @@
+/*****************************************************************************
+ *                        Shapeways, Inc Copyright (c) 2011
+ *                               Java Source
+ *
+ * This source is licensed under the GNU LGPL v2.1
+ * Please read http://www.gnu.org/copyleft/lgpl.html for more information
+ *
+ * This software comes with the standard NO WARRANTY disclaimer for any
+ * purpose. Use it at your own risk. If there's a problem you get to fix it.
+ *
+ ****************************************************************************/
+
+package abfab3d.creator;
+
+// External Imports
+import java.util.*;
+
+// Internal Imports
+
+/**
+ * Create a user interface for an editor.
+ *
+ * @author Alan Hudson
+ */
+public interface UserInterfaceCreator {
+    /**
+     * Create a user interface for a kernal.
+     *
+     * @param dir The directory to place the files
+     * @param genParams Parameters for generation
+     * @param kernal The kernal
+     * @param remove The parameters to remove
+     */
+    public void createInterface(String dir, Map<String,String> genParams, GeometryKernal kernal, Set<String> remove);
+}
--- src/java/abfab3d/creator/ui/JavaStandaloneUICreator.java
+++ src/java/abfab3d/creator/ui/JavaStandaloneUICreator.java
@@ -25,6 +25,9 @@ import abfab3d.creator.*;
  * @author Alan Hudson
  */
 public class JavaStandaloneUICreator {
+    /** The type of editors */
+    private enum Editors {TEXTFIELD, COMBOBOX, FILE_DIALOG};
+
     private HashMap<Integer,String> indentCache;
 
     private GeometryKernal kernal;
@@ -87,7 +90,7 @@ System.out.println("Creating Java Interface");
             ps.println("    public void setupUI() {");
 
 
-            ps.println(indent(8) + "GridLayout layout = new GridLayout(" + (params.size() - remove.size() + 1) + ",2);");
+            ps.println(indent(8) + "GridLayout layout = new GridLayout(" + (params.size() - remove.size() + 1) + ",3);");
             ps.println(indent(8) + "setLayout(layout);");
             ps.println();
 
@@ -130,8 +133,40 @@ System.out.println("Creating Java Interface");
 System.out.println("Adding param: " + p.getName());
         ps.println(indent(8) + "JLabel " + p.getName() + "Label = new JLabel(\"" + p.getName() + "\");");
         ps.println(indent(8) + "getContentPane().add(" + p.getName() + "Label);");
-        ps.println(indent(8) + p.getName() + "Editor = new JTextField(\"" + p.getDefaultValue() + "\");");
+
+        switch(getEditor(p)) {
+            case COMBOBOX:
+                ps.println(indent(8) + "String[] " + p.getName() + "Enums = new String[] {");
+                String[] vals = p.getEnumValues();
+
+                ps.print(indent(12));
+                for(int i=0; i < vals.length; i++) {
+                    ps.print("\"" + vals[i] + "\"");
+                    if (i < vals.length - 1) {
+                        ps.print(",");
+                    }
+                }
+                ps.println("};");
+                ps.println(indent(8) + p.getName() + "Editor = new JComboBox(" + p.getName() + "Enums);");
+                break;
+            case FILE_DIALOG:
+                ps.println(indent(8) + p.getName() + "Dialog = new JFileChooser(new File(\".\"));");
+                ps.println(indent(8) + p.getName() + "Button = new JButton(\"Browse\");");
+                ps.println(indent(8) + p.getName() + "Button.addActionListener(this);");
+            default:
+                ps.println(indent(8) + p.getName() + "Editor = new JTextField(\"" + p.getDefaultValue() + "\");");
+        }
         ps.println(indent(8) + "getContentPane().add(" + p.getName() + "Editor);");
+
+        // Determine third column content
+        switch(getEditor(p)) {
+            case FILE_DIALOG:
+                ps.println(indent(8) + "getContentPane().add(" + p.getName() + "Button);");
+                break;
+            default:
+                ps.println(indent(8) + "getContentPane().add(new JLabel(\"\"));");
+        }
+
         ps.println();
 
     }
@@ -143,16 +178,23 @@ System.out.println("Adding param: " + p.getName());
      * @param p The parameter
      */
     private void addGlobalButtons(PrintStream ps) {
-        ps.println(indent(8) + "JButton submitButton = new JButton(\"Submit\");");
+        ps.println(indent(8) + "submitButton = new JButton(\"Submit\");");
         ps.println(indent(8) + "getContentPane().add(submitButton);");
         ps.println(indent(8) + "submitButton.addActionListener(this);");
+
+        ps.println(indent(8) + "getContentPane().add(new JLabel(\"\"));");
+
+        ps.println(indent(8) + "uploadButton = new JButton(\"Upload\");");
+        ps.println(indent(8) + "getContentPane().add(uploadButton);");
+        ps.println(indent(8) + "uploadButton.addActionListener(this);");
     }
 
     private void addActions(PrintStream ps, Map<String,Parameter> params, Set<String> remove) {
 
         // submit button
         ps.println(indent(4) + "public void actionPerformed(ActionEvent e) {");
-        ps.println(indent(8) + "// Get all params to global string vars");
+        ps.println(indent(8) + "if (e.getSource() == submitButton) {");
+        ps.println(indent(12) + "// Get all params to global string vars");
 
         Iterator<Parameter> itr = params.values().iterator();
 
@@ -162,18 +204,29 @@ System.out.println("Adding param: " + p.getName());
                 continue;
             }
 
-            ps.println(indent(8) + p.getName() + " = ((JTextField)" + p.getName() + "Editor).getText();");
+            switch(getEditor(p)) {
+                case TEXTFIELD:
+                    ps.println(indent(12) + p.getName() + " = ((JTextField)" + p.getName() + "Editor).getText();");
+                    break;
+                case FILE_DIALOG:
+                    ps.println(indent(12) + p.getName() + " = ((JTextField)" + p.getName() + "Editor).getText();");
+                    break;
+                case COMBOBOX:
+                    ps.println(indent(12) + p.getName() + " = (String) ((JComboBox)" + p.getName() + "Editor).getSelectedItem();");
+                    break;
+            }
+
         }
 
         ps.println();
 
         // Create Kernal
         String class_name = kernal.getClass().getName();
-        ps.println(indent(8) + class_name + " kernal = new " + class_name + "();");
+        ps.println(indent(12) + class_name + " kernal = new " + class_name + "();");
 
         // Put params into a map
 
-        ps.println(indent(8) + "HashMap<String,String> params = new HashMap<String,String>();");
+        ps.println(indent(12) + "HashMap<String,String> params = new HashMap<String,String>();");
         itr = params.values().iterator();
 
         while(itr.hasNext()) {
@@ -182,19 +235,51 @@ System.out.println("Adding param: " + p.getName());
                 continue;
             }
 
-            ps.println(indent(8) + "params.put(\"" + p.getName() + "\", " + p.getName() + ");");
+            ps.println(indent(12) + "params.put(\"" + p.getName() + "\", " + p.getName() + ");");
         }
 
-        ps.println(indent(8) + "Map<String,Object> parsed_params = ParameterUtil.parseParams(kernal.getParams(), params);");
+        ps.println(indent(12) + "Map<String,Object> parsed_params = ParameterUtil.parseParams(kernal.getParams(), params);");
 
         // Generate Geometry
-        ps.println(indent(8) + "try {");
-        ps.println(indent(12) + "FileOutputStream fos = new FileOutputStream(\"out.x3db\");");
-        ps.println(indent(12) + "BufferedOutputStream bos = new BufferedOutputStream(fos);");
-        ps.println(indent(12) + "kernal.generate(parsed_params, bos);");
-        ps.println(indent(12) + "fos.close();");
-        ps.println(indent(8) + "} catch(IOException ioe) { ioe.printStackTrace(); }");
-        ps.println(indent(8) + "System.out.println(\"Model Done\");");
+        ps.println(indent(12) + "try {");
+        ps.println(indent(16) + "FileOutputStream fos = new FileOutputStream(\"out.x3db\");");
+        ps.println(indent(16) + "BufferedOutputStream bos = new BufferedOutputStream(fos);");
+        ps.println(indent(16) + "kernal.generate(parsed_params, bos);");
+        ps.println(indent(16) + "fos.close();");
+        ps.println(indent(12) + "} catch(IOException ioe) { ioe.printStackTrace(); }");
+        ps.println(indent(12) + "System.out.println(\"Model Done\");");
+
+        ps.println(indent(8) + "} else if (e.getSource() == uploadButton) {");
+        ps.println(indent(12) + "System.out.println(\"Uploading Model\");");
+        //ps.println(indent(8) + "}");
+
+
+        itr = params.values().iterator();
+        boolean more_elses = false;
+
+        while(itr.hasNext()) {
+            Parameter p = itr.next();
+            if (remove.contains(p.getName())) {
+                continue;
+            }
+
+            if (getEditor(p) == Editors.FILE_DIALOG) {
+                ps.println(indent(8) + "} else if (e.getSource() == " + p.getName() + "Button) {");
+
+                ps.println(indent(12) + "int returnVal = " + p.getName() + "Dialog" + ".showOpenDialog(this);");
+
+                ps.println(indent(12) + "if (returnVal == JFileChooser.APPROVE_OPTION) {");
+                ps.println(indent(16) + "File file = " + p.getName() + "Dialog" + ".getSelectedFile();");
+                ps.println(indent(16) + " ((JTextField)" + p.getName() + "Editor).setText(file.toString());");
+                ps.println(indent(12) + "}");
+                more_elses = true;
+            }
+        }
+
+        if (more_elses) {
+            ps.println(indent(8) + "}");
+        }
+
         ps.println(indent(4) + "}");
     }
 
@@ -203,6 +288,9 @@ System.out.println("Adding param: " + p.getName());
      */
     private void addGlobalVars(PrintStream ps, Map<String,Parameter> params, Set<String> remove) {
             Iterator<Parameter> itr = params.values().iterator();
+
+        ps.println("JButton submitButton;");
+        ps.println("JButton uploadButton;");
 
         while(itr.hasNext()) {
             Parameter p = itr.next();
@@ -215,6 +303,13 @@ System.out.println("Adding param: " + p.getName());
             ps.println(indent(4) + "/** " + p.getDesc() + " Editor */");
             ps.println(indent(4) + "protected JComponent " + p.getName() + "Editor;");
             ps.println();
+
+            switch(getEditor(p)) {
+                case FILE_DIALOG:
+                    ps.println(indent(4) + "protected JButton " + p.getName() + "Button;");
+                    ps.println(indent(4) + "protected JFileChooser " + p.getName() + "Dialog;");
+                    break;
+            }
         }
 
         ps.println();
@@ -235,5 +330,20 @@ System.out.println("Adding param: " + p.getName());
         }
 
         return ret_val;
+    }
+
+    /**
+     * Get the editor type to use.
+     */
+    private Editors getEditor(Parameter p) {
+        if (p.getEditorType() == Parameter.EditorType.FILE_DIALOG) {
+            return Editors.FILE_DIALOG;
+        }
+
+        if (p.getDataType() == Parameter.DataType.ENUM) {
+            return Editors.COMBOBOX;
+        }
+
+        return Editors.TEXTFIELD;
     }
 }
