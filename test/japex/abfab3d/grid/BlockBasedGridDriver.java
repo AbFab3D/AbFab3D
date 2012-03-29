f2e45363a75703005a1c3b08bfc1def4cae329b8
giles7777 <giles7777@gmail.com>
2012-03-29 14:03:47 -0700

    Added japex timing of grids

---
+++ test/japex/abfab3d/grid/BlockBasedGridDriver.java
@@ -0,0 +1,29 @@
+package abfab3d.grid;
+
+import com.sun.japex.TestCase;
+
+/**
+ * TODO: Add docs
+ *
+ * @author Alan Hudson
+ */
+public class BlockBasedGridDriver extends BaseGridDriver {
+    double voxel_size = 0.001;
+    double slice_height = 0.001;
+
+    public void allocate(TestCase testCase) {
+        super.prepare(testCase);
+
+        int w = Integer.parseInt(testCase.getParam("width"));
+        int h = Integer.parseInt(testCase.getParam("height"));
+        int d = Integer.parseInt(testCase.getParam("depth"));
+
+        grid =  new BlockBasedGridByte(w,h,d,voxel_size, slice_height);
+
+        String input = testCase.getParam("input");
+
+        if (input.equals("ReadRandom")) {
+            writeRandom(grid, 0.1f);
+        }
+    }
+}
