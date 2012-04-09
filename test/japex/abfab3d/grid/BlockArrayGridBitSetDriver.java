e13fed77729da38e4002f171cb65bacc28488800
GrayJ <grayje@uw.edu>
2012-04-09 15:57:38 -0700

    Test files for recent Block types.

---
+++ test/japex/abfab3d/grid/BlockArrayGridBitSetDriver.java
@@ -0,0 +1,18 @@
+package abfab3d.grid;
+
+import com.sun.japex.TestCase;
+
+/**
+ * TODO: Add docs
+ *
+ * @author Alan Hudson
+ */
+public class BlockArrayGridBitSetDriver extends BaseGridDriver {
+    public void allocate(TestCase testCase) {
+        int w = Integer.parseInt(testCase.getParam("width"));
+        int h = Integer.parseInt(testCase.getParam("height"));
+        int d = Integer.parseInt(testCase.getParam("depth"));
+
+        grid =  new BlockArrayGrid(w,h,d,voxel_size, slice_height, new int[] {4,4,4}, BlockArrayGrid.BlockType.BitSet);
+    }
+}
