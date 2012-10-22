15bd0c261145340a9b01846feeaf91237aa31f6e
giles7777 <giles7777@gmail.com>
2012-10-22 14:07:05 -0700

    Added new x3d viewer

--- apps/common/src/java/app/common/GridSaver.java
+++ apps/common/src/java/app/common/GridSaver.java
@@ -21,6 +21,8 @@ import abfab3d.mesh.MeshDecimator;
 import abfab3d.mesh.WingedEdgeTriangleMesh;
 
 import java.io.IOException;
+import java.io.OutputStream;
+
 import static java.lang.System.currentTimeMillis;
 import static abfab3d.util.Output.fmt;
 import static abfab3d.util.Output.printf;
@@ -32,21 +34,24 @@ import static abfab3d.util.Output.printf;
  */
 public class GridSaver {
     /**
-     * Write a grid using the IsoSurfaceMaker to an stl file
+     * Write a grid using the IsoSurfaceMaker to the specified file
      *
      * @param grid
      * @param smoothSteps
      * @param maxCollapseError
      * @throws IOException
      */
-    public static void writeIsosurfaceMakerSTL(String filename, Grid grid, int smoothSteps, double maxCollapseError) throws IOException {
+    public static void writeIsosurfaceMaker(String filename, Grid grid, int smoothSteps, double maxCollapseError) throws IOException {
         int nx = grid.getWidth();
         int ny = grid.getHeight();
         int nz = grid.getDepth();
         double vs = grid.getVoxelSize();
 
+
         double gbounds[] = new double[]{-nx*vs/2,nx*vs/2,-ny*vs/2,ny*vs/2,-nz*vs/2,nz*vs/2};
         double ibounds[] = extendBounds(gbounds, -vs/2);
+
+        String encoding = filename.substring(filename.lastIndexOf(".")+1);
 
         IsosurfaceMaker im = new IsosurfaceMaker();
         im.setIsovalue(0.);
@@ -68,10 +73,10 @@ public class GridSaver {
         System.out.println("***Smoothing mesh");
         long t0 = currentTimeMillis();
         //for(int i = 0; i < smoothSteps; i++){
-            printf("smoothMesh()\n");
-            t0 = currentTimeMillis();
-            ls.processMesh(mesh, smoothSteps);
-            printf("mesh processed: %d ms\n",(currentTimeMillis() - t0));
+        printf("smoothMesh()\n");
+        t0 = currentTimeMillis();
+        ls.processMesh(mesh, smoothSteps);
+        printf("mesh processed: %d ms\n",(currentTimeMillis() - t0));
         //}
 
         int fcount = faces.length;
@@ -102,7 +107,104 @@ public class GridSaver {
             System.out.println("Final face count: " + fcount);
             System.out.println("Decimate time: " + (System.currentTimeMillis() - start_time));
         }
-        MeshExporter.writeMeshSTL(mesh, fmt(filename, fcount));
+
+        if (encoding.equals("stl")) {
+            MeshExporter.writeMeshSTL(mesh, fmt(filename, fcount));
+        } else if (encoding.startsWith("x3d")) {
+            MeshExporter.writeMesh(mesh, fmt(filename, fcount));
+        } else {
+            throw new IllegalArgumentException("Unsupported file format: " + encoding);
+        }
+    }
+
+    /**
+     * Write a grid using the IsoSurfaceMaker to the specified file
+     *
+     * @param grid
+     * @param smoothSteps
+     * @param maxCollapseError
+     * @throws IOException
+     */
+    public static void writeIsosurfaceMaker(Grid grid, OutputStream os, String encoding, int smoothSteps, double maxCollapseError) throws IOException {
+        int nx = grid.getWidth();
+        int ny = grid.getHeight();
+        int nz = grid.getDepth();
+        double vs = grid.getVoxelSize();
+
+
+        double gbounds[] = new double[]{-nx*vs/2,nx*vs/2,-ny*vs/2,ny*vs/2,-nz*vs/2,nz*vs/2};
+        double ibounds[] = extendBounds(gbounds, -vs/2);
+
+        IsosurfaceMaker im = new IsosurfaceMaker();
+        im.setIsovalue(0.);
+        im.setBounds(ibounds);
+        im.setGridSize(nx, ny, nz);
+
+        IndexedTriangleSetBuilder its = new IndexedTriangleSetBuilder();
+
+        im.makeIsosurface(new IsosurfaceMaker.SliceGrid(grid, gbounds, 0), its);
+        int[][] faces = its.getFaces();
+        WingedEdgeTriangleMesh mesh = new WingedEdgeTriangleMesh(its.getVertices(), faces);
+
+        double centerWeight = 1.0; // any non negative value is OK
+
+        LaplasianSmooth ls = new LaplasianSmooth();
+
+        ls.setCenterWeight(centerWeight);
+
+        System.out.println("***Smoothing mesh");
+        long t0 = currentTimeMillis();
+        //for(int i = 0; i < smoothSteps; i++){
+        printf("smoothMesh()\n");
+        t0 = currentTimeMillis();
+        ls.processMesh(mesh, smoothSteps);
+        printf("mesh processed: %d ms\n",(currentTimeMillis() - t0));
+        //}
+
+        int fcount = faces.length;
+
+        if (maxCollapseError > 0) {
+            MeshDecimator md = new MeshDecimator();
+            md.setMaxCollapseError(maxCollapseError);
+            long start_time = System.currentTimeMillis();
+
+            int target = mesh.getTriangleCount() / 4;
+            int current = fcount;
+            System.out.println("Original face count: " + fcount);
+
+            while(true) {
+                target = mesh.getTriangleCount() / 2;
+                System.out.println("Target face count : " + target);
+                md.processMesh(mesh, target);
+
+                current = mesh.getFaceCount();
+                System.out.println("Current face count: " + current);
+                if (current > target * 1.25) {
+                    // not worth continuing
+                    break;
+                }
+            }
+
+            fcount = mesh.getFaceCount();
+            System.out.println("Final face count: " + fcount);
+            System.out.println("Decimate time: " + (System.currentTimeMillis() - start_time));
+        }
+
+        double max_axis = Math.max(grid.getHeight() * grid.getSliceHeight(), grid.getWidth() * grid.getVoxelSize());
+        max_axis = Math.max(max_axis, grid.getDepth() * grid.getVoxelSize());
+
+        double z = 2 * max_axis / Math.tan(Math.PI / 4);
+        float[] pos = new float[] {0,0,(float) z};
+
+        if (encoding.equals("stl")) {
+            // TODO: Need to implement streaming version
+            throw new IllegalArgumentException("Unsupported file format: " + encoding);
+            //MeshExporter.writeMeshSTL(mesh, os, encoding);
+        } else if (encoding.startsWith("x3d")) {
+            MeshExporter.writeMesh(mesh, os, encoding, pos);
+        } else {
+            throw new IllegalArgumentException("Unsupported file format: " + encoding);
+        }
     }
 
     /**
---
+++ apps/common/src/java/app/common/X3DViewer.java
@@ -0,0 +1,102 @@
+/*****************************************************************************
+ *                        Shapeways, Inc Copyright (c) 2011
+ *                               Java Source
+ *
+ * This source is licensed under the GNU LGPL v2.1
+ * Please read http://www.gnu.org/copyleft/lgpl.html for more information
+ *
+ * This software comes with the standard NO WARRANTY disclaimer for any
+ * purpose. Use it at your own risk. If there's a problem you get to fix it.
+ *
+ ****************************************************************************/
+
+package app.common;
+
+import abfab3d.grid.Grid;
+import abfab3d.io.output.IsosurfaceMaker;
+import abfab3d.io.output.MeshExporter;
+import abfab3d.mesh.IndexedTriangleSetBuilder;
+import abfab3d.mesh.LaplasianSmooth;
+import abfab3d.mesh.MeshDecimator;
+import abfab3d.mesh.WingedEdgeTriangleMesh;
+
+import java.awt.*;
+import java.io.*;
+
+import static abfab3d.util.Output.fmt;
+import static abfab3d.util.Output.printf;
+import static java.lang.System.currentTimeMillis;
+import org.apache.commons.io.FileUtils;
+
+/**
+ * Common code for viewing X3D files.
+ *
+ * @author Alan Hudson
+ */
+public class X3DViewer {
+    /**
+     * View an X3D file using an external X3DOM player
+     *
+     * @param grid
+     * @param smoothSteps
+     * @param maxDecimateError
+     * @throws IOException
+     */
+    public static void viewX3DOM(Grid grid, int smoothSteps, double maxDecimateError) throws IOException {
+        // TODO: Make thread safe using tempDir
+        String dest = "/tmp/ringpopper/";
+        File dir = new File(dest);
+        dir.mkdirs();
+
+        String pf = "x3dom/x3dom.css";
+        String dest_pf = dest + pf;
+        File dest_file = new File(dest_pf);
+        File src_file = new File("src/html/" + pf);
+        if (!dest_file.exists()) {
+            System.out.println("Copying file: " + src_file + " to dir: " + dest);
+            FileUtils.copyFile(src_file, dest_file, true);
+        }
+
+        pf = "x3dom/x3dom.js";
+        dest_pf = dest + pf;
+        dest_file = new File(dest_pf);
+        src_file = new File("src/html/" + pf);
+        if (!dest_file.exists()) {
+            System.out.println("Copying file: " + src_file + " to dir: " + dest);
+            FileUtils.copyFile(src_file, dest_file, true);
+        }
+        File f = new File("/tmp/ringpopper/out.xhtml");
+        FileOutputStream fos = new FileOutputStream(f);
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+        PrintStream ps = new PrintStream(bos);
+
+
+        try {
+            ps.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
+            ps.println("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
+            ps.println("<head>");
+            ps.println("<meta http-equiv=\"X-UA-Compatible\" content=\"chrome=1\" />");
+            ps.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />");
+            ps.println("<title>Ring Popper Demo</title>");
+//            ps.println("<link rel='stylesheet' type='text/css' href='http://www.x3dom.org/x3dom/release/x3dom.css'></link>");
+            ps.println("<link rel='stylesheet' type='text/css' href='x3dom/x3dom.css'></link>");
+            ps.println("</head>");
+            ps.println("<body>");
+
+            ps.println("<p class='case'>");
+            GridSaver.writeIsosurfaceMaker(grid,bos, "x3d", smoothSteps, maxDecimateError);
+
+//            ps.println("<script type='text/javascript' src='http://www.x3dom.org/x3dom/release/x3dom.js'></script>");
+            ps.println("<script type='text/javascript' src='x3dom/x3dom.js'></script>");
+
+            ps.println("</p>");
+            ps.println("</body></html>");
+        } finally {
+            bos.flush();
+            bos.close();
+            fos.close();
+        }
+
+        Desktop.getDesktop().browse(f.toURI());
+    }
+}
