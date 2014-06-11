18c8a3e9e15134abbc287c3c57a1d4483c854d30
Vladimir Bulatov <vladimir@bulatov.org>
2014-06-11 10:27:33 -0700

    fixed logic for in DensityGridExtractor to deal with interior/exterior

--- src/java/abfab3d/grid/op/DensityGridExtractor.java
+++ src/java/abfab3d/grid/op/DensityGridExtractor.java
@@ -37,15 +37,27 @@ public class DensityGridExtractor implements AttributeOperation {
 
     /** The number of sub-voxel distance levels used in the distanceGrid */
     private int subvoxelResolution;
-    
+    // this is temprorary hack to store max in and out not calculated values of distance grid 
+    // the actual default values may be different. They may depend in actual distance grid implementation  
+    static final int DEFAULT_IN_VALUE = -Short.MAX_VALUE;
+    static final int DEFAULT_OUT_VALUE = Short.MAX_VALUE;
      
     /**
-       if inDistanceLevel 
-     */
-    public DensityGridExtractor(double inDistanceValue, double outDistanceValue, AttributeGrid distanceGrid,
+       the extractor 
+       inDistance - value for internal surface
+       outDistace - value for external surface 
+       distanceGrid - input distance grid to be used for density extraction 
+       maxInDistance - maximal interior distance actualy stored in the distance grid
+       maxOutDistance - maximal exterior distance stored in the distance grid
+                       these params are used only to make correct threshold. 
+       if inDistance < maxInDistance the object will have no internal surface
+       if outDistance > maxOutDistance the density grid will have no exterior surfaceas 
+       
+    */
+    public DensityGridExtractor(double inDistance, double outDistance, AttributeGrid distanceGrid,
                                 double maxInDistance, double maxOutDistance, int subvoxelResolution) {
-        this.inDistanceValue = inDistanceValue;
-        this.outDistanceValue = outDistanceValue;
+        this.inDistanceValue = inDistance;
+        this.outDistanceValue = outDistance;
         this.distanceGrid = distanceGrid;
         this.maxInDistance = maxInDistance;
         this.maxOutDistance = maxOutDistance;
@@ -69,22 +81,28 @@ public class DensityGridExtractor implements AttributeOperation {
         int nz = distanceGrid.getDepth();
 
         // 5 intervals for distance values
-
+        // -INF,  inDistanceMinus, inDistancePlus, outDistanceMinus, outDistancePlus, +INF
         int inDistanceMinus = (int) (inDistanceValue * subvoxelResolution / vs - subvoxelResolution / 2);
         int inDistancePlus = (int) (inDistanceValue * subvoxelResolution / vs + subvoxelResolution / 2);
         int outDistanceMinus = (int) (outDistanceValue * subvoxelResolution / vs - subvoxelResolution / 2);
         int outDistancePlus = (int) (outDistanceValue * subvoxelResolution / vs + subvoxelResolution / 2);
-
+        if(inDistanceValue < maxInDistance) {
+            // no interior shell will be generated 
+            inDistanceMinus = inDistancePlus = DEFAULT_IN_VALUE;
+        } 
+        if(outDistanceValue > maxOutDistance){
+            // no exterior shell will be generated 
+            outDistanceMinus = outDistancePlus = DEFAULT_OUT_VALUE;
+        }
         for(int y=0; y < ny; y++) {
             for(int x=0; x < nx; x++) {
                 for(int z=0; z < nz; z++) {
                     long att = (long) (short) distanceGrid.getAttribute(x,y,z);
 
                     short dest_att;
-
+                    
                     if (att < inDistanceMinus) {
-                        dest.setState(x,y,z,Grid.OUTSIDE);
-                        //dest.setData(x,y,z,Grid.INSIDE, subvoxelResolution);
+                        dest.setData(x,y,z,Grid.OUTSIDE, subvoxelResolution);
                     } else if (att >= inDistanceMinus && att < inDistancePlus) {
                         dest_att = (short) (att - inDistanceMinus);
                         dest.setData(x,y,z, Grid.INSIDE,dest_att);
@@ -95,7 +113,7 @@ public class DensityGridExtractor implements AttributeOperation {
                         dest_att = (short) (outDistancePlus - att);
                         dest.setData(x,y,z, Grid.INSIDE,dest_att);
                     } else {
-                        dest.setState(x,y,z,Grid.OUTSIDE);
+                        dest.setData(x,y,z,Grid.OUTSIDE, 0);
                     }
                 }
             }
