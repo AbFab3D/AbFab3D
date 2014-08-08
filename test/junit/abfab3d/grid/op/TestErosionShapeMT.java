549c03fe84fcf19c3e076711c6540f449f3e33e8
Vladimir Bulatov <vladimir@bulatov.org>
2014-08-08 08:53:15 -0700

    utils in ImageUtils added

--- test/junit/abfab3d/grid/op/TestErosionShapeMT.java
+++ test/junit/abfab3d/grid/op/TestErosionShapeMT.java
@@ -48,7 +48,7 @@ public class TestErosionShapeMT extends TestCase {
 
     public void testBlockErosion() {
 
-        int nx = 100, ny = 150, nz = 200;
+        int nx = 50, ny = 60, nz = 70;
 
         int offset = 10;
 
@@ -183,6 +183,7 @@ public class TestErosionShapeMT extends TestCase {
 
     public static void main(String[] args) {
 
+        new TestErosionShapeMT().testBlockErosion();
 
     }
 }
