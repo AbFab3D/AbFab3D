db0bc5c2d8abf3c5e60918e28d05da258dfec70c
giles <giles7777@gmail.com>
2014-09-05 09:15:41 -0700

    Stubb for reading voxel files.

---
+++ src/java/abfab3d/io/input/SVXReader.java
@@ -0,0 +1,28 @@
+/*****************************************************************************
+ *                        Shapeways, Inc Copyright (c) 2014
+ *                               Java Source
+ *
+ * This source is licensed under the GNU LGPL v2.1
+ * Please read http://www.gnu.org/copyleft/lgpl.html for more information
+ *
+ * This software comes with the standard NO WARRANTY disclaimer for any
+ * purpose. Use it at your own risk. If there's a problem you get to fix it.
+ *
+ ****************************************************************************/
+
+package abfab3d.io.input;
+
+import abfab3d.grid.AttributeGrid;
+
+/**
+ * SVX Reader.
+ *
+ * Reads the Shapeways voxel transmital format.
+ *
+ * @author Alan Hudson
+ */
+public class SVXReader {
+    public AttributeGrid load(String file) {
+        return null;
+    }
+}
