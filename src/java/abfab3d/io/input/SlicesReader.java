25673b3f52d08a8230ccddf895ded634a747e890
Vladimir Bulatov <vladimir@bulatov.org>
2014-09-05 09:26:19 -0700

    Stub for SlicesReader

---
+++ src/java/abfab3d/io/input/SlicesReader.java
@@ -0,0 +1,38 @@
+/*****************************************************************************
+ *                        Shapeways, Inc Copyright (c) 2014
+ *                               Java Source
+ *
+ * This source is licensed under the GNU LGPL v2.1
+ * Please read http://www.gnu.org/copyleft/lgpl.html for more information
+ *
+ * This software comes with the standard NO WARRANTY disclaimer for any
+ * purpose. Use it at your own risk. If there's a problem you get to fix it.
+ *
+ ****************************************************************************/
+
+package abfab3d.io.input;
+
+import abfab3d.grid.AttributeGrid;
+
+/**
+ * Slces Reader.
+ *
+ * Reads the a bunch of spices images into a grid
+ *
+ * @author Vladimir Bulatov
+ */
+public class SVXReader {
+
+    String fileTemplate = "";
+
+    /**
+       reads a set of PNG image files into a grid 
+       start first 
+     */
+    public int readSlices(AttributeGrid grid, String fileTemplate, 
+                          int fromIndex, int toIndex, int sliceCount) throws IOException {
+        
+        return 0;
+        
+    }
+}
