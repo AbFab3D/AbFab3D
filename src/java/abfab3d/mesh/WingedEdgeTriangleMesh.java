11b2749b1cbdcd3fd886beaedc3872e7afc26d2b
Vladimir Bulatov <vladimir@bulatov.org>
2014-10-18 17:15:06 -0700

    added missing imports

--- src/java/abfab3d/mesh/WingedEdgeTriangleMesh.java
+++ src/java/abfab3d/mesh/WingedEdgeTriangleMesh.java
@@ -16,7 +16,11 @@ import abfab3d.util.StructSet;
 import abfab3d.util.StructMixedData;
 import abfab3d.util.StructMap;
 import abfab3d.util.TriangleCollector;
-//import abfab3d.util.DefaultHashFunction;
+import abfab3d.util.TriangleMesh;
+import abfab3d.util.EdgeCollapseParams;
+import abfab3d.util.EdgeCollapseResult;
+import abfab3d.util.FaceFlipChecker;
+
 
 import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
