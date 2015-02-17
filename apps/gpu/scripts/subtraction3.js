d70f1caa04d1031200c4c2ca64b89ab48eb4890d
Vladimir Bulatov <vladimir@bulatov.org>
2015-02-17 12:18:44 -0800

    test of complex subtraction

---
+++ apps/gpu/scripts/subtraction3.js
@@ -0,0 +1,22 @@
+function cross3D(size, thickeness){
+    var union = new Union();
+    var boxX = new Box(0,0,0,size,thickeness, thickeness);
+    var boxY = new Box(0,0,0, thickeness, size, thickeness);
+    var boxZ = new Box(0,0,0,thickeness, thickeness,size);
+    union.add(boxX);
+    union.add(boxY);
+    union.add(boxZ);
+    return union;
+}
+
+function main(args) {
+    var size = 30*MM;
+    var thickness = 10*MM;
+	var b = 25*MM;
+    var grid = createGrid(-16*MM,16*MM,-16*MM,16*MM,-16*MM,16*MM,0.1*MM);
+    var diff = new Subtraction(cross3D(size, thickness), cross3D(size, 0.7*thickness));
+    var maker = new GridMaker();
+    maker.setSource(diff);
+    maker.makeGrid(grid);
+    return grid;
+}
