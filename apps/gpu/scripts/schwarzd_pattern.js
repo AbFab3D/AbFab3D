19055748ee5d26b19aa836d7dbca0848c634465a
giles <giles7777@gmail.com>
2015-03-19 16:20:24 -0700

    Fixed params

--- apps/gpu/scripts/schwarzd_pattern.js
+++ apps/gpu/scripts/schwarzd_pattern.js
@@ -1,11 +1,11 @@
-var params = [
+var uiParams = [
     {
-        id: "thickness",
+        name: "thickness",
         desc: "Thickness",
-        type: "range",
-        "rangeMin": 0.005,
-        "rangeMax": 0.05,
-        "step": 0.001,
+        type: "double",
+        rangeMin: 0.005,
+        rangeMax: 0.05,
+        step: 0.001,
         defaultVal: 0.01
     }
 ];
@@ -14,17 +14,17 @@ function main(args) {
 
     var base = new Sphere(radius);
     var pattern = new VolumePatterns.SchwarzD();
-	pattern.set("period", 0.5*radius);
-	pattern.set("thickness", 0.5*MM);
-	pattern.set("level", 0);
-	
+    pattern.set("period", 0.5*radius);
+    pattern.set("thickness", 0.5*MM);
+    pattern.set("level", 0);
+
     var shape = new Embossing(base, pattern);
-	shape.set("minValue", -0.4*MM);
-	shape.set("maxValue", 0.*MM);
-	shape.set("factor", 0.8);
-	shape.set("blend", 0.5*MM);
-	
-	var r = radius+1*MM;
-	return new Shape(shape,new Bounds(-r,r,-r,r,-r,r));
+    shape.set("minValue", -0.4*MM);
+    shape.set("maxValue", 0.0*MM);
+    shape.set("factor", 0.8);
+    shape.set("blend", 0.5*MM);
+
+    var r = radius+1*MM;
+    return new Shape(shape,new Bounds(-r,r,-r,r,-r,r));
 
 }
