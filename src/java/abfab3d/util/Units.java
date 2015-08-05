33de1e8120589875d321a25f01481e92ed2c9622
giles <giles7777@gmail.com>
2015-08-05 14:45:42 -0700

    Added CM^3 value

--- src/java/abfab3d/util/Units.java
+++ src/java/abfab3d/util/Units.java
@@ -22,6 +22,7 @@ public class Units{
     static public final double CM = 0.01; // cm -> meters
     static public final double MM = 0.001; // mm -> meters
     static public final double MM3 = 1.E-9; // mm^3 -> meters^3
+    static public final double CM3 = 1.E-6; // cm^3 -> meters^3
     static public final double FT = 0.304; // ft -> meters
     static public final double IN = 0.0254; // inches -> meters
     static public final double UM = 1.e-6; // micron -> meters
