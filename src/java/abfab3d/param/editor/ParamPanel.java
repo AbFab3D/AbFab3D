4b73fd084cd7cad584e43b8840698cc1cd6f28c3
giles <giles7777@gmail.com>
2016-02-26 10:12:15 -0800

    Basic mock out

---
+++ src/java/abfab3d/param/editor/ParamPanel.java
@@ -0,0 +1,29 @@
+/*****************************************************************************
+ *                        Shapeways, Inc Copyright (c) 2016
+ *                               Java Source
+ *
+ * This source is licensed under the GNU LGPL v2.1
+ * Please read http://www.gnu.org/copyleft/lgpl.html for more information
+ *
+ * This software comes with the standard NO WARRANTY disclaimer for any
+ * purpose. Use it at your own risk. If there's a problem you get to fix it.
+ *
+ ****************************************************************************/
+package abfab3d.param.editor;
+
+import abfab3d.param.Parameterizable;
+
+import java.awt.*;
+
+/**
+ * Creates an editing panel for a parameterizable
+ *
+ * @author Alan Hudson
+ */
+public class ParamPanel {
+    private Parameterizable m_param;
+    public Component ParamPanel(Parameterizable param) {
+        m_param = param;
+        return null;
+    }
+}
