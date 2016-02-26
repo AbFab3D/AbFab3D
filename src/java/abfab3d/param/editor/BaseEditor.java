cfda860d01dfaef4ebe400588ab8eeb798aecde9
giles <giles7777@gmail.com>
2016-02-26 11:15:34 -0800

    Fixed listener

--- src/java/abfab3d/param/editor/BaseEditor.java
+++ src/java/abfab3d/param/editor/BaseEditor.java
@@ -6,13 +6,13 @@ package abfab3d.param.editor;
  * @author Alan Hudson
  */
 public abstract class BaseEditor implements Editor {
-    private ChangeListener m_listener;
+    private ParamChangedListener m_listener;
 
     /**
      * Get notification of any parameter changes from this editor
      * @param l
      */
-    public void addChangeListener(ChangeListener l) {
+    public void addChangeListener(ParamChangedListener l) {
         m_listener = l;
     }
 }
