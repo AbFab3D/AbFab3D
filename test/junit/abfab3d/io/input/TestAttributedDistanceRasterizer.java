bfd7bfcec02fa2cdc3844dadddf980c412751dbb
Vladimir Bulatov <vladimir@bulatov.org>
2016-06-21 10:49:53 -0700

    adding tests

--- test/junit/abfab3d/io/input/TestAttributedDistanceRasterizer.java
+++ test/junit/abfab3d/io/input/TestAttributedDistanceRasterizer.java
@@ -22,6 +22,10 @@ import abfab3d.util.BoundingBoxCalculator;
 import abfab3d.util.Bounds;
 import abfab3d.util.DataSource;
 import abfab3d.util.Vec;
+
+import abfab3d.io.output.GridSaver;
+
+
 import junit.framework.TestCase;
 
 import java.io.IOException;
@@ -55,16 +59,50 @@ public class TestAttributedDistanceRasterizer extends TestCase {
         printf("size: %d\n",size);
         AttributedDistanceRasterizer rasterizer = new AttributedDistanceRasterizer(bounds, size,size,size);
         rasterizer.setDataDimension(3);
-
+        
         AttributeGrid grid = new ArrayAttributeGridByte(bounds,vs,vs);
         GridDataDesc attDesc = new GridDataDesc();
-        GridDataChannel gdc = new GridDataChannel(GridDataChannel.DISTANCE, "dist", 16,-1*MM,1*MM);
+        GridDataChannel gdc = new GridDataChannel(GridDataChannel.DISTANCE, "dist", 8,0.5*MM,-0.5*MM);
         attDesc.addChannel(gdc);
         grid.setDataDesc(attDesc);
 
         rasterizer.getAttributedDistances(box, colorizer, grid);
 
         GridUtil.printSliceAttribute(grid,size/2);
+    }
+
+    public void devTestBox() throws IOException {
+
+        double bx = 10*MM;
+        double by = 10*MM;
+        double bz = 10*MM;
+
+        double s  = Math.max(bx,Math.max(by,bz));
+
+        AttributedMesh box = makeBox(bx,by,bz);
+        Bounds bounds = box.getBounds();
+        Constant colorizer = new Constant(0.9,0.2,0.2);
+        colorizer.initialize();
+
+        GridLoader loader = new GridLoader();
+        loader.setThreadCount(8);
+        loader.setMaxInDistance(1*MM);
+        loader.setMaxOutDistance(1*MM);           
+        loader.setMargins(0.2*MM);   
+        loader.setPreferredVoxelSize(0.1*MM);   
+
+        AttributeGrid grid = loader.rasterizeAttributedTriangles(box, colorizer);
+        GridSaver writer = new GridSaver();
+        writer.setWriteTexturedMesh(true);
+        writer.setTexPixelSize(1);
+        writer.setMeshSmoothingWidth(1);
+        writer.setTexTriExt(1.5);
+        writer.setTexTriGap(1.5);
+        //String outPath = "/tmp/tex/box.svx";
+        String outPath = "/tmp/tex/box.x3d";
+        printf("writing: %s\n", outPath);
+        writer.write(grid, outPath);
+        printf("done\n");        
     }
 
     private AttributedMesh makeBox(double bx,double by,double bz) {
@@ -95,4 +133,11 @@ public class TestAttributedDistanceRasterizer extends TestCase {
         return box;
 
     }
+
+    public static void main(String arg[])throws Exception {
+        new TestAttributedDistanceRasterizer().devTestBox();
+    }
+
 }
+
+
