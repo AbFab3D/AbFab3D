9430e38b01dd01829cda6b6fbbae8f934278b988
giles <giles7777@gmail.com>
2016-10-18 10:44:07 -0700

    Add transform to shape

--- src/java/abfab3d/param/Shape.java
+++ src/java/abfab3d/param/Shape.java
@@ -5,6 +5,13 @@ import abfab3d.core.Initializable;
 import abfab3d.core.Material;
 import abfab3d.core.MaterialShader;
 import abfab3d.core.ResultCodes;
+import abfab3d.core.VecTransform;
+import abfab3d.transforms.CompositeTransform;
+import abfab3d.transforms.TransformsFactory;
+
+import java.util.List;
+
+import static abfab3d.core.Output.printf;
 
 /**
  * A ShapeJS Shape.  A shape contains a datasource to generate the geometry, a material specification
@@ -17,10 +24,14 @@ public class Shape extends BaseParameterizable implements Initializable {
     private Material m_material;
     private MaterialShader m_shader;
     private int m_id;
+    // transformation which is applied to the data point before the calculation of data value
+    protected VecTransform m_transform = null;
 
     protected SNodeParameter mp_source = new SNodeParameter("source");
     protected SNodeParameter mp_material = new SNodeParameter("material");
     protected SNodeParameter mp_shader = new SNodeParameter("shader");
+    SNodeListParameter mp_transform = new SNodeListParameter("transform", new BaseSNodeFactory(TransformsFactory.getNames(), TransformsFactory.getClassNames()));
+
 
     Parameter m_aparam[] = new Parameter[]{
             mp_source, mp_material, mp_shader
@@ -74,10 +85,50 @@ public class Shape extends BaseParameterizable implements Initializable {
         return m_id;
     }
 
+    /**
+     * Transform the data source
+     * @noRefGuide
+     * @param transform General transformation to apply to the object before it is rendered
+     */
+    public void setTransform(VecTransform transform){
+        mp_transform.set((Parameterizable)transform);
+    }
+
+    /**
+     * @noRefGuide
+     @return Transformation the object
+     */
+    public VecTransform getTransform() {
+        return m_transform;
+    }
+
+    public VecTransform makeTransform() {
+
+        List list = mp_transform.getValue();
+
+        Object tr[] = list.toArray();
+        if(tr.length == 0){
+            return null;
+        } else if(tr.length == 1){
+            return (VecTransform)tr[0];
+        } else {
+            CompositeTransform ct = new CompositeTransform();
+            for(int k = 0 ; k < tr.length; k++){
+                ct.add((VecTransform)tr[k]);
+            }
+            return ct;
+        }
+    }
+
     @Override
     public int initialize() {
         if (m_source instanceof Initializable) {
             ((Initializable)m_source).initialize();
+        }
+
+        m_transform = makeTransform();
+        if(m_transform != null && m_transform  instanceof Initializable){
+            ((Initializable)m_transform).initialize();
         }
 
         return ResultCodes.RESULT_OK;
