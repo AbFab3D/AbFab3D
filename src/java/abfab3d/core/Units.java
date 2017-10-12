7fc7dd322f267292ebd4e45d3a5136133ef35954
giles <giles7777@gmail.com>
2017-10-12 11:27:46 -0400

    Added unit name

--- src/java/abfab3d/core/Units.java
+++ src/java/abfab3d/core/Units.java
@@ -18,7 +18,6 @@ package abfab3d.core;
  * @author Vladimir Bulatov
  */
 public class Units{
-    
     static public final double CM = 0.01; // cm -> meters
     static public final double MM = 0.001; // mm -> meters
     static public final double MM3 = 1.E-9; // mm^3 -> meters^3
@@ -33,4 +32,63 @@ public class Units{
     static public final double TODEGREE = 180/Math.PI; // degree to radians    
     static public final double NS2MS = 1.e-6; // conversion from nano to milliseconds
 
+    /**
+     * Helper for converting units
+     *
+     * @param val  The value in inUnit units
+     * @param inUnit
+     * @param outUnit
+     * @return The value in outUnit units
+     */
+    public double convertToUnit(double val, UnitName inUnit, UnitName outUnit) {
+        double mval;
+
+        switch(inUnit) {
+            case M:
+                mval = val;
+                break;
+            case CM:
+                mval = val / CM;
+                break;
+            case MM:
+                mval = val / MM;
+                break;
+            case M2:
+                mval = val;
+                break;
+            case CM2:
+                mval = val / CM2;
+                break;
+            case M3:
+                mval = val;
+                break;
+            case MM3:
+                mval = val / MM3;
+                break;
+            case CM3:
+                mval = val / CM3;
+                break;
+            default: throw new IllegalArgumentException("Unsupported unit: " + inUnit);
+        }
+
+        switch(outUnit) {
+            case M:
+                return mval;
+            case CM:
+                return mval * CM;
+            case MM:
+                return mval * MM;
+            case M2:
+                return mval;
+            case CM2:
+                return mval * CM2;
+            case M3:
+                return mval;
+            case MM3:
+                return mval * MM3;
+            case CM3:
+                return mval * CM3;
+            default: throw new IllegalArgumentException("Unsupported unit: " + inUnit);
+        }
+    }
 }
