1bf161ef1a4a945672922b58c95087f8873dcb52
Vladimir Bulatov <vladimir@bulatov.org>
2018-08-10 09:35:13 -0700

    added variable dimension to PointSetArray

--- src/java/abfab3d/util/PointSetArray.java
+++ src/java/abfab3d/util/PointSetArray.java
@@ -32,17 +32,17 @@ public class PointSetArray implements TriangleProducer, PointSet  {
     public static final int SHAPE_OCTAHEDRON = 0; 
     public static final int SHAPE_SPHERE = 1; 
 
-    int m_shapeType = SHAPE_OCTAHEDRON;
-    int m_subdivisionLevel = 1;
+    protected int m_shapeType = SHAPE_OCTAHEDRON;
+    protected int m_subdivisionLevel = 1;
     
     // points are represented via 3 coordinates 
     double coord[] = null;
 
-    int m_size=0; // points count 
-    int m_arrayCapacity = 0;
-    
+    protected int m_size=0; // points count 
+    protected int m_arrayCapacity = 0;
+    protected int m_dataDimension =3;
     // size of geometrical shape to represent each point 
-    double pointSize = 0.05*MM;
+    protected double pointSize = 0.05*MM;
     
     /**
        makes empty point cloud.
@@ -54,11 +54,18 @@ public class PointSetArray implements TriangleProducer, PointSet  {
 
     public PointSetArray(int initialCapacity){
 
+        this(initialCapacity,3);                
+
+    }
+
+    public PointSetArray(int initialCapacity, int dataDimension){
+
         // avoid wrong behavior 
         if(initialCapacity < 1) initialCapacity = 1;
-            
+        m_dataDimension = dataDimension;
+
         m_arrayCapacity = initialCapacity;
-        coord = new double[3*m_arrayCapacity];
+        coord = new double[m_dataDimension*m_arrayCapacity];
         
     }
 
@@ -67,7 +74,19 @@ public class PointSetArray implements TriangleProducer, PointSet  {
      */
     public PointSetArray(double coord[]){
         this.coord = coord;
-        m_arrayCapacity = coord.length/3;
+        m_arrayCapacity = coord.length/m_dataDimension;
+        m_size = m_arrayCapacity;
+
+    }
+    
+    /**
+       accept coordinates as flat array of double
+     */
+    public PointSetArray(double coord[], int dataDimension){
+
+        this.coord = coord;
+        m_dataDimension = dataDimension;
+        m_arrayCapacity = coord.length/m_dataDimension;
         m_size = m_arrayCapacity;
 
     }
@@ -78,7 +97,7 @@ public class PointSetArray implements TriangleProducer, PointSet  {
     public PointSetArray(Vector<Vector3d> points){
         
         this.coord = getCoord(points);
-        m_size = coord.length/3;
+        m_size = coord.length/m_dataDimension;
         m_arrayCapacity = m_size;
     }
 
@@ -89,7 +108,7 @@ public class PointSetArray implements TriangleProducer, PointSet  {
     public void clear() {
         m_size = 0;
         m_arrayCapacity = 10;
-        coord = new double[3*m_arrayCapacity];
+        coord = new double[m_dataDimension*m_arrayCapacity];
     }
 
     /**
@@ -124,15 +143,44 @@ public class PointSetArray implements TriangleProducer, PointSet  {
         if(m_size >= m_arrayCapacity)
             reallocArray();
 
-        int start = m_size*3;
+        int start = m_size*m_dataDimension;
         coord[start] = x;
         coord[start+1]= y;
         coord[start+2]= z;
         m_size++;
     }
 
+    public final void addPoint(double x, double y, double z, double w){
+
+        if(m_size >= m_arrayCapacity)
+            reallocArray();
+
+        int start = m_size*m_dataDimension;
+        coord[start] = x;
+        coord[start+1]= y;
+        coord[start+2]= z;
+        coord[start+3]= w;
+        m_size++;
+    }
+
     public final void addPoint(Tuple3d pnt){
         addPoint(pnt.x,pnt.y,pnt.z);
+    }
+
+    public final void addPoint(double pnt[]){
+        if(m_dataDimension == 3){
+            addPoint(pnt[0],pnt[1],pnt[2]); 
+            return;
+        }
+        if(m_size >= m_arrayCapacity)
+            reallocArray();
+
+        int start = m_size*m_dataDimension;
+        for(int i = 0; i < m_dataDimension; i++){            
+            coord[start++] = pnt[i];
+        }
+        m_size++;
+        
     }
 
     /**
@@ -151,13 +199,27 @@ public class PointSetArray implements TriangleProducer, PointSet  {
      */
     public final void addPoints(PointSet pnts){
 
-        Vector3d p = new Vector3d();
+        if(pnts instanceof PointSetArray){
+            addPointSetArray((PointSetArray)pnts);
+            return;
+        }
 
+        Vector3d p = new Vector3d();
         for(int k = 0; k < pnts.size(); k++){
             pnts.getPoint(k, p);
             addPoint(p.x,p.y,p.z);
         }
     }
+
+    public final void addPointSetArray(PointSetArray pnts){
+
+        double pnt[] = new double[m_dataDimension];
+        for(int k = 0; k < pnts.size(); k++){
+            pnts.getPoint(k, pnt);
+            addPoint(pnt);
+        }        
+    }
+
 
     public boolean getTriangles(TriangleCollector collector){
         switch(m_shapeType){
@@ -175,7 +237,7 @@ public class PointSetArray implements TriangleProducer, PointSet  {
         Octa shape = new Octa(pointSize/2);
         
         for(int i = 0; i < count; i++){
-            int start  = i*3;
+            int start  = i*m_dataDimension;
             double 
                 x = coord[start],
                 y = coord[start+1],
@@ -192,7 +254,7 @@ public class PointSetArray implements TriangleProducer, PointSet  {
         
         for(int i = 0; i < count; i++){
 
-            int start  = i*3;
+            int start  = i*m_dataDimension;
             double 
                 x = coord[start],
                 y = coord[start+1],
@@ -214,9 +276,17 @@ public class PointSetArray implements TriangleProducer, PointSet  {
 
     }
 
+    public void getPoint(int index, double point[]){
+
+        int j = index*m_dataDimension;
+        for(int i = 0; i < m_dataDimension; i++,j++){
+            point[i] = coord[j];
+        }
+    }
+
     public void getPoint(int index, Tuple3d point){
 
-        int start = index*3;
+        int start = index*m_dataDimension;
         point.x = coord[start];
         point.y = coord[start+1];
         point.z = coord[start+2];
@@ -225,7 +295,7 @@ public class PointSetArray implements TriangleProducer, PointSet  {
 
     public void setPoint(int index, Tuple3d point){
 
-        int start = index*3;
+        int start = index*m_dataDimension;
         coord[start] = point.x;
         coord[start+1] = point.y;
         coord[start+2] = point.z;
@@ -235,7 +305,7 @@ public class PointSetArray implements TriangleProducer, PointSet  {
     private void reallocArray(){
 
         int ncapacity = 2*m_arrayCapacity;
-        double ncoord[] = new double[ncapacity*3];
+        double ncoord[] = new double[ncapacity*m_dataDimension];
         System.arraycopy(coord, 0, ncoord,0,coord.length);
         m_arrayCapacity = ncapacity;
         coord = ncoord;
@@ -243,13 +313,13 @@ public class PointSetArray implements TriangleProducer, PointSet  {
     }
 
 
-    private static double [] getCoord(Vector<Vector3d> points){
+    private double [] getCoord(Vector<Vector3d> points){
 
         int count = points.size();
-        double coord[] = new double[count*3];
+        double coord[] = new double[count*m_dataDimension];
         for(int i = 0; i < count; i++){
             Vector3d pnt = points.get(i);
-            int i3 = i*3;
+            int i3 = i*m_dataDimension;
             coord[i3] = pnt.x;
             coord[i3+1] = pnt.y;
             coord[i3+2] = pnt.z;
